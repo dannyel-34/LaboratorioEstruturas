8e3ec1ea1b15399b38fe98b4a02b7631c3760a59
salak403 <salak403@salak403-5.itmsalas.edu.co>
2017-08-11 09:58:23 -0500

    adicionamos cambios en la clase2

--- src/Cls2/Practica2.java
+++ src/Cls2/Practica2.java
@@ -13,20 +13,51 @@ import javax.swing.JOptionPane;
  */
 public class Practica2 {
 
-    int[] vec=new int[10];
-    
+    int[] vec = new int[10];
+
     public static void main(String[] args) {
         Practica2 obj = new Practica2();
         obj.ingresar();
-
+        obj.imprimir();
+        ///JOptionPane.showMessageDialog(null, obj.buscarMayor());
+        obj.imprimir2(obj.buscarMayor());//instancio el objecto alcargar 
     }
-    
-    private  void ingresar()
-    {
-        
-                                                                    
-        for (int i = 1; i <= 10; i++) {
-            vec[i]=Integer.parseInt(JOptionPane.showInputDialog("Digite codigo \t: "));
+
+    private void ingresar() {
+
+        for (int i = 0; i < 10; i++) {
+            vec[i] = Integer.parseInt(JOptionPane.showInputDialog("Digite codigo \t: "));
         }
     }
+
+    private void imprimir() {
+        String cadena = "";
+        for (int i = 0; i < 10; i++) {
+            cadena += vec[i] + " | ";
+
+        }
+
+        JOptionPane.showMessageDialog(null, cadena);
+    }
+
+    private int buscarMayor() {
+        int mayor = vec[0];
+
+        for (int i = 0; i < 10; i++) {
+
+            if (vec[i] > mayor) {
+                mayor = vec[i];
+            }
+
+        }
+
+        return mayor;
+    }
+
+    private void imprimir2(int dato)
+    {
+        JOptionPane.showMessageDialog(null, dato);
+    }
+    
+    
 }
