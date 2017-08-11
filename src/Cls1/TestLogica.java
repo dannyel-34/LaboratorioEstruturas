15c4ceaab28e1959bf79ad020e317f37340c4961
Daniellecaalle <daniellecalle@hotmail.com>
2017-08-10 20:33:30 -0500

    Mi Primer commit actulizando los clases y archivos del proyecto

--- src/Cls1/TestLogica.java
+++ src/Cls1/TestLogica.java
@@ -5,44 +5,42 @@
  */
 package Cls1;
 
-import javax.swing.JOptionPane;
+import javax.swing.JOptionPane;//Instruccion Que Nos importa la liberia o (clase) a utilizar el metodo JOptionPane
 
 /**
  *
- * @author Usuario
+ * @author Daniel P.Calle
+ * @version 04-08-2017/1.0
  */
+
 public class TestLogica {
 
+    //Atributos Privados 
+    private static String nombre;//Declaramos el atributo nombre como string
+    private static int precio, cantidad;//Declaramos el atributo precio, cantidad como Numerico(Entero): Int
+
+    //Metodo Principal    
     public static void main(String[] args) {
-
-        String nombre;
-
-        int precio, cantidad, mayor = 0, mayor2 = 0;
-
+        int mayor = 0, vrPrecio = 0;//Declaramos y inicializamos las variables locales mayor, vrPrecio en cero
+        String nom = "";//declaramos La variable local nombre como string(Caracter) secuencia de expresioens alfanumericas
+        
+        //Ciclo for 
         for (int i = 1; i <= 3; i++) {
             nombre = JOptionPane.showInputDialog(null, "Digite Nombre: ");
             precio = Integer.parseInt(JOptionPane.showInputDialog("Digite Precio: "));
             cantidad = Integer.parseInt(JOptionPane.showInputDialog("Digite Cantidad: "));
-            mayor2 = cantidad;
-            mayor = validarMayor(cantidad);
 
-            if (mayor >= mayor2) {
-                JOptionPane.showMessageDialog(null, "Nombre\t: " + nombre + " " + "Precio\t: " + precio + " " + "Cantidad\t: " + cantidad);
+            //Condicional Siempre que valida la mayor cantidad en los registro
+            if (cantidad > mayor) {
+                mayor = cantidad;//A mayor le asignamos el valor cantidad actual
+                nom = nombre;//a nom le llevamos el nombre del producto
+                vrPrecio = precio;//a vrPrecio le llevamos el precio actual del producto
+
             }
 
         }
+        //Instruccion Que Nos imprime Solo El registro cuya cantidad validada sea mayor. Uitlizamos JOptionPane de clase swing
+        JOptionPane.showMessageDialog(null, "Nombre\t: " + nom + " " + " Cantidad\t: " + mayor + " " + " Precio\t: " + vrPrecio);
 
     }
-
-    public static int validarMayor(int cant) {
-        int mayorCant = 0;
-
-        if (cant > mayorCant) {
-            mayorCant = cant;
-
-        }
-
-        return mayorCant;
-    }
-
 }
